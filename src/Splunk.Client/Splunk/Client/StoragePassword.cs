deef4eb302ed155ba1c8e4ea87a4e2a128fb37fc
David Noble <dnoble@splunk.com>
2014-04-25 00:46:39 -0700

    A bit better than stubbed StoragePassword support.
    Next: methods, properties, and documentation.

---
+++ src/Splunk.Client/Splunk/Client/StoragePassword.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Copyright 2014 Splunk, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+// TODO:
+// [O] Contracts
+// [O] Documentation
+// [ ] Properties & Methods
+
+namespace Splunk.Client
+{
+    /// <summary>
+    /// Provides a class that represents a Splunk storage password resource.
+    /// </summary>
+    public class StoragePassword : Entity<StoragePassword>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StoragePassword"/> class.
+        /// </summary>
+        /// <param name="context">
+        /// An object representing a Splunk server session.
+        /// </param>
+        /// <param name="namespace">
+        /// An object identifying a Splunk service namespace.
+        /// </param>
+        /// <param name="collection">
+        /// </param>
+        /// <param name="name">
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// <see cref="name"/> is <c>null</c> or empty.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// <see cref="context"/> or <see cref="namespace"/> are <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <see cref="namespace"/> is not specific.
+        /// </exception>
+        internal StoragePassword(Context context, Namespace @namespace, string name)
+            : base(context, @namespace, StoragePasswordCollection.ClassResourceName, name)
+        { }
+
+        public StoragePassword()
+        { }
+
+        #endregion
+    }
+}
---
+++ src/Splunk.Client/Splunk/Client/StoragePasswordCollection.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Copyright 2014 Splunk, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+// TODO:
+// [ ] Documentation
+
+namespace Splunk.Client
+{
+    /// <summary>
+    /// Provides a class that represents a collection of Splunk Application
+    /// resources.
+    /// </summary>
+    public class ApplicationCollection : EntityCollection<ApplicationCollection, Application>
+    {
+        #region Constructors
+
+        internal ApplicationCollection(Context context, Namespace @namespace, ApplicationCollectionArgs args = null)
+            : base(context, @namespace, ClassResourceName, args)
+        { }
+
+        public ApplicationCollection()
+        { }
+
+        #endregion
+
+        #region Privates/internals
+
+        internal static readonly ResourceName ClassResourceName = new ResourceName("apps", "local");
+
+        #endregion
+    }
+}

a9a394a8074211de35656e72eb613dbab809ffa2
David Noble <dnoble@splunk.com>
2014-05-01 14:57:08 -0700

    JobArgs is now composed of nullable value types

--- src/Splunk.Client/Splunk/Client/JobArgs.cs
+++ src/Splunk.Client/Splunk/Client/JobArgs.cs
@@ -14,8 +14,9 @@
  * under the License.
  */
 
-// TODO:
-// [ ]  Documentation
+//// TODO:
+//// [ ]  Documentation
+//// [ ] Class for specifying a time string.
 
 namespace Splunk.Client
 {
@@ -38,78 +39,58 @@ namespace Splunk.Client
     /// </remarks>
     public sealed class JobArgs : Args<JobArgs>
     {
-        #region Constructors
-
-        public JobArgs()
-        { }
-
-        #endregion
-
         #region Properties
 
         [DataMember(Name = "auto_cancel", EmitDefaultValue = false)]
-        [DefaultValue(0)]
-        public int AutoCancel
+        public int? AutoCancel
         { get; set; }
         
         [DataMember(Name = "auto_finalize_ec", EmitDefaultValue = false)]
-        [DefaultValue(0)]
-        public int AutoFinalizeEventCount
+        public int? AutoFinalizeEventCount
         { get; set; }
 
         [DataMember(Name = "auto_pause", EmitDefaultValue = false)]
-        [DefaultValue(0)]
-        public int AutoPause
+        public int? AutoPause
         { get; set; }
 
         [DataMember(Name = "earliest_time", EmitDefaultValue = false)]
-        [DefaultValue(null)]
-        public string EarliestTime // TODO: Convenience class for specifying a time string. See 
+        public string EarliestTime
         { get; set; }
 
         [DataMember(Name = "enable_lookups", EmitDefaultValue = false)]
-        [DefaultValue(true)]
-        public bool EnableLookups
+        public bool? EnableLookups
         { get; set; }
 
         [DataMember(Name = "exec_mode", EmitDefaultValue = false)]
-        [DefaultValue(typeof(ExecutionMode), "Normal")]
-        public ExecutionMode ExecutionMode
+        public ExecutionMode? ExecutionMode
         { get; set; }
 
         [DataMember(Name = "force_bundle_replication", EmitDefaultValue = false)]
-        [DefaultValue(false)]
-        public bool ForceBundleReplication
+        public bool? ForceBundleReplication
         { get; set; }
 
         [DataMember(Name = "id", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string Id
         { get; set; }
 
         [DataMember(Name = "index_earliest", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string IndexEarliest
         { get; set; }
 
         [DataMember(Name = "index_latest", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string IndexLatest
         { get; set; }
 
         [DataMember(Name = "latest_time", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string LatestTime
         { get; set; }
 
         [DataMember(Name = "max_count", EmitDefaultValue = false)]
-        [DefaultValue(10000)]
-        public int MaxCount
+        public int? MaxCount
         { get; set; }
 
         [DataMember(Name = "max_time", EmitDefaultValue = false)]
-        [DefaultValue(0)]
-        public int MaxTime
+        public int? MaxTime
         { get; set; }
 
         /// <summary>
@@ -120,93 +101,75 @@ namespace Splunk.Client
         /// /services/apps/local endpoint.
         /// </remarks>
         [DataMember(Name = "namespace", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string Namespace
         { get; set; }
 
         [DataMember(Name = "now", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string Now
         { get; set; }
 
         [DataMember(Name = "reduce_freq", EmitDefaultValue = false)]
-        [DefaultValue(0)]
-        public int ReduceFrequency
+        public int? ReduceFrequency
         { get; set; }
 
         [DataMember(Name = "reload_macros", EmitDefaultValue = false)]
-        [DefaultValue(true)]
-        public bool ReloadMacros
+        public bool? ReloadMacros
         { get; set; }
 
         [DataMember(Name = "remote_server_list", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string RemoteServerList
         { get; set; }
 
         [DataMember(Name = "rf", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public IReadOnlyList<string> RequiredFieldList
         { get; set; }
 
         [DataMember(Name = "reuse_max_seconds_ago", EmitDefaultValue = false)]
-        [DefaultValue(0)]
-        public int ReuseMaxSecondsAgo
+        public int? ReuseMaxSecondsAgo
         { get; set; }
 
         [DataMember(Name = "rt_blocking", EmitDefaultValue = false)]
-        [DefaultValue(false)]
-        public bool RealTimeBlocking
+        public bool? RealTimeBlocking
         { get; set; }
 
         [DataMember(Name = "rt_indexfilter", EmitDefaultValue = false)]
-        [DefaultValue(false)]
-        public bool RealTimeIndexFilter
+        public bool? RealTimeIndexFilter
         { get; set; }
 
         [DataMember(Name = "rt_maxblocksecs", EmitDefaultValue = false)]
-        [DefaultValue(60)]
-        public int RealTimeMaxBlockSeconds
+        public int? RealTimeMaxBlockSeconds
         { get; set; }
 
         [DataMember(Name = "rt_queue_size", EmitDefaultValue = false)]
-        [DefaultValue(10000)]
-        public int RealTimeQueueSize
+        public int? RealTimeQueueSize
         { get; set; }
 
         [DataMember(Name = "search_listener", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string SearchListener
         { get; set; }
 
         [DataMember(Name = "search_mode", EmitDefaultValue = false)]
-        [DefaultValue(typeof(SearchMode), "Normal")]
-        public SearchMode SearchMode
+        public SearchMode? SearchMode
         { get; set; }
 
         [DataMember(Name = "spawn_process", EmitDefaultValue = false)]
-        [DefaultValue(true)]
-        public bool SpawnProcess
+        public bool? SpawnProcess
         { get; set; }
 
         [DataMember(Name = "status_buckets", EmitDefaultValue = false)]
-        [DefaultValue(0)]
-        public int StatusBuckets
+        public int? StatusBuckets
         { get; set; }
 
         [DataMember(Name = "sync_bundle_replication", EmitDefaultValue = false)]
-        [DefaultValue(false)]
-        public bool SyncBundleReplication
+        public bool? SyncBundleReplication
         { get; set; }
 
         [DataMember(Name = "time_format", EmitDefaultValue = false)]
-        [DefaultValue(null)]
         public string TimeFormat
         { get; set; }
 
         [DataMember(Name = "timeout", EmitDefaultValue = false)]
-        [DefaultValue(86400)]
-        public int Timeout
+        public int? Timeout
         { get; set; }
 
         #endregion

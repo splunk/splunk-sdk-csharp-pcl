9cc7234fb538763415a114faac1d3b03d6593f33
Glenn Block <glenn.block@gmail.com>
2014-06-18 18:10:06 -0700

    Cleaning up settings label, and adding port to settings

--- Windows8StoreApp/SearchApp/SearchApp/MainPage.xaml.cs
+++ Windows8StoreApp/SearchApp/SearchApp/MainPage.xaml.cs
@@ -145,7 +145,8 @@ namespace SplunkSearch
                 ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
 
                 host = localSettings.Values["host"].ToString();
-                port = int.Parse(localSettings.Values["port"].ToString());
+                var portString = localSettings.Values["port"].ToString();
+                port = int.Parse(portString);
                 if (string.Equals(localSettings.Values["scheme"].ToString(), "Http", StringComparison.OrdinalIgnoreCase))
                 {
                     schem = Scheme.Http;

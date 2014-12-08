8fb7c7ac5029e6fc18c5d0951e738966734ef710
Shakeel Mohamed <smohamed@splunk.com>
2014-12-08 13:59:07 -0800

    Bump version numbers for 2.1 release

--- src/Splunk.Client/Properties/AssemblyInfo.cs
+++ src/Splunk.Client/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 
 //// Version information
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]

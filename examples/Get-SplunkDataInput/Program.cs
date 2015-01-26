e8899429d5ea985d79f6d66467fdd6df6ab90ca4
David Noble <DNoble@splunk.com>
2015-01-25 16:02:10 -0800

    Checkpoint for safe keeping

--- examples/Get-SplunkDataInput/Program.cs
+++ examples/Get-SplunkDataInput/Program.cs
@@ -53,42 +53,49 @@ namespace Splunk.Examples.GetSplunkDataInputs
         /// <returns>a task</returns>
         static async Task Run(Service service)
         {
-            await service.LogOnAsync(SdkHelper.Splunk.Username, SdkHelper.Splunk.Password);
-
-            Console.WriteLine("Data inputs:");
-
-            var collection = service.CreateEntityCollection("data", "inputs", "all");
-            int count = 0;
-            await collection.GetAllAsync();
-
-            //// TODO:
-            //// [X] 1. Make Entity<TResource>.Content public (It still must be assigned to a dynamic variable before use.)
-            //// [X] 2. Write a convenience method on the Service object for getting an EntityCollection on some path: service.CreateEntityCollection("data", "inputs", "all")
-            //// [X] 3. Write a convenience method on the Service object for getting an Entity on some path: something like service.CreateEntity("data", "inputs", "tcp", "ssl")
-            //// [X] 4. Add service.CreateEntityCollectionCollection for completeness.
-            //// [X] 5. Revert the access change to EntityCollection<Entity<TResource>, TResource>(service, new ResourceName("data", "inputs", "all")) because (2) makes it unnecessary.
-            //// [X] 6. Revert the access change to Entity<TResource>(service, new ResourceName("data", "inputs", "all")) because (3) makes it unnecessary.
-            //// [O] 7. Show a data input in action within this loop.
-            //// [ ] 8. Fill holes in IService (holes are unrelated to this exercise)
-
-            foreach (var entity in collection)
+            try
             {
-                Console.WriteLine("{0:D5}. {1}", ++count, entity.Id);
-                dynamic dataInput = entity.Content;
+                await service.LogOnAsync(SdkHelper.Splunk.Username, SdkHelper.Splunk.Password);
 
-                Console.WriteLine("       Disabled: {0}", dataInput.Disabled);
-                Console.WriteLine("          Index: {0}", dataInput.Index);
-                Console.WriteLine("           Type: {0}", dataInput.Eai.Type);
+                Console.WriteLine("Data inputs:");
 
-                if (dataInput.Eai.Type == "example_hydra_worker")
+                var collection = service.CreateEntityCollection("data", "inputs", "all");
+                int count = 0;
+                await collection.GetAllAsync();
+
+                //// TODO:
+                //// [X] 1. Make Entity<TResource>.Content public (It still must be assigned to a dynamic variable before use.)
+                //// [X] 2. Write a convenience method on the Service object for getting an EntityCollection on some path: service.CreateEntityCollection("data", "inputs", "all")
+                //// [X] 3. Write a convenience method on the Service object for getting an Entity on some path: something like service.CreateEntity("data", "inputs", "tcp", "ssl")
+                //// [X] 4. Add service.CreateEntityCollectionCollection for completeness.
+                //// [X] 5. Revert the access change to EntityCollection<Entity<TResource>, TResource>(service, new ResourceName("data", "inputs", "all")) because (2) makes it unnecessary.
+                //// [X] 6. Revert the access change to Entity<TResource>(service, new ResourceName("data", "inputs", "all")) because (3) makes it unnecessary.
+                //// [ ] 7. Enliven the example.
+                //// [ ] 8. Fill holes in IService (holes are unrelated to this exercise)
+
+                foreach (var entity in collection)
                 {
-                    // Restart...
-                    await entity.InvokeAsync("disable");
-                    await entity.InvokeAsync("enable");
-                }
-            }
+                    Console.WriteLine("{0:D5}. {1}", ++count, entity.Id);
+                    dynamic dataInput = entity.Content;
 
-            await service.LogOffAsync();
+                    Console.WriteLine("       Disabled: {0}", dataInput.Disabled);
+                    Console.WriteLine("          Index: {0}", dataInput.Index);
+                    Console.WriteLine("           Type: {0}", dataInput.Eai.Type);
+
+                    if (dataInput.Eai.Type == "example_hydra_scheduler")
+                    {
+                        // Restart...
+                        await entity.InvokeAsync("disable");
+                        await entity.InvokeAsync("enable");
+                    }
+                }
+
+                await service.LogOffAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
